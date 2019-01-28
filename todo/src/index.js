e595752004871d30de92c91dcdea732184beb48e
Moonkaman <nickbatmanglidj@gmail.com>
2019-01-28 16:17:19 -0700

    Create store in index.js & create rootReducer

--- todo/src/index.js
+++ todo/src/index.js
@@ -1,7 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
 
-import './index.css';
+import rootReducer from './reducers';
+
 import App from './App';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import './index.css';
+
+const store = createStore(rootReducer);
+
+ReactDOM.render(<Provider store={store}><App /></Provider>, document.getElementById('root'));
