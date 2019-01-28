5d1d9fb36ed7072ef0a73316a144071f2f78c416
Moonkaman <nickbatmanglidj@gmail.com>
2019-01-28 16:23:50 -0700

    Create components needed to create todolist component

---
+++ todo/src/components/TodoContainer.jsx
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+
+export default class TodoContainer extends Component {
+  state = {
+    todoInput: ""
+  };
+
+  render() {
+    return (
+      <div>
+        <h1>TodoContainer</h1>
+      </div>
+    );
+  }
+}
