a5d9882b4ff3544daf01fbfb0ea8b81fc5007810
Moonkaman <nickbatmanglidj@gmail.com>
2019-01-29 15:54:41 -0700

    Use localstorage to persist the data using eggheads tutorial

--- todo/src/index.js
+++ todo/src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { createStore } from 'redux';
 import { Provider } from 'react-redux';
+import { loadState, saveState } from './localstorage';
 
 import rootReducer from './reducers';
 
@@ -9,6 +10,11 @@ import App from './App';
 
 import './index.css';
 
-const store = createStore(rootReducer);
+const persistedState = loadState();
+const store = createStore(rootReducer,persistedState);
+
+store.subscribe(_ => {
+  saveState(store.getState());
+})
 
 ReactDOM.render(<Provider store={store}><App /></Provider>, document.getElementById('root'));
